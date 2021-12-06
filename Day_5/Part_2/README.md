c8370b69615d133ef7ab0921861485ea4c741668
Jordan Williams <jtw0213@gmail.com>
2021-12-06 08:23:57 -0600

    Add readme to each part to make repo easier to view

---
+++ Day_5/Part_2/README.md
@@ -0,0 +1,3 @@
+# Advent Of Code 2021 Day 5: Hydrothermal Venture Part 2
+
+TBD
--- templates/createDay.sh
+++ templates/createDay.sh
@@ -7,5 +7,10 @@ cp -r part_template ../Day_$DAY/Part_1/
 cp -r part_template ../Day_$DAY/Part_2/
 
 touch ../Day_$DAY/README.md
+touch ../Day_$DAY/Part_1/README.md
+touch ../Day_$DAY/Part_2/README.md
 
 echo "# Advent Of Code 2021 Day $DAY: $TITLE\n\n## Part 1\nTBD\n\n---\n## Part 2\nTBD" > ../Day_$DAY/README.md
+
+echo "# Advent Of Code 2021 Day $DAY: $TITLE Part 1\n\nTBD" > ../Day_$DAY/Part_1/README.md
+echo "# Advent Of Code 2021 Day $DAY: $TITLE Part 2\n\nTBD" > ../Day_$DAY/Part_2/README.md
