5a1ff8e630e4cf2f8e6fda88f24dfe4d11df0696
Jordan Williams <jtw0213@gmail.com>
2021-12-09 18:42:15 -0600

    Finish Day 8 Part 1

--- Day_8/Part_1/main.py
+++ Day_8/Part_1/main.py
@@ -1,5 +1,18 @@
 def main():
-    pass
+    with open('input.txt') as f:
+        lines = [line.rstrip() for line in f.readlines()]
+
+    numOfVals = 0
+
+    for line in lines:
+        # Ignore the unique pattern for part 1
+        _, outputVal = line.split(' | ')
+
+        for val in outputVal.split():
+            if len(val) in [2, 3, 4, 7]:
+                numOfVals += 1
+
+    print(f"Numer of appearences: {numOfVals}")
 
 
 if __name__ == "__main__":
