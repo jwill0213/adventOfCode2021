04ae75b093e4f29fb4621d65f87e34cf51e343c6
Jordan Williams <jtw0213@gmail.com>
2021-12-18 01:32:20 -0600

    Day 12 WIP

--- Day_12/main.py
+++ Day_12/main.py
@@ -1,5 +1,25 @@
 def part1(lines):
-    pass
+    startingPoints = [l for l in lines if "start" in l]
+
+    paths = []
+    for point in startingPoints:
+        p1, p2 = point.split('-')
+        if p1 == 'start':
+            paths.append([(p1, p2)])
+        else:
+            paths.append([(p2, p1)])
+
+    newPaths = []
+    for path in paths:
+        currPoint = path[-1][1]
+        possibleMoves = [tuple(m.split('-')) for m in lines if currPoint in m]
+        for move in possibleMoves:
+            if move in path:
+                pass
+        print(currPoint, possibleMoves)
+
+    print(startingPoints)
+    print(paths)
 
 
 def part2(lines):
@@ -8,7 +28,7 @@ def part2(lines):
 
 if __name__ == "__main__":
 
-    with open('input.txt') as f:
+    with open('test_input.txt') as f:
         lines = [line.rstrip() for line in f.readlines()]
 
     print("\n----- Part 1 -----\n")
